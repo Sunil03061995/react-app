70d07358071c5e2e23839b76e6b5641d37a97e8c
Sunil03061995 <119438951+Sunil03061995@users.noreply.github.com>
2022-12-01 21:25:45 +0530

    bugs resolved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -79,14 +79,14 @@ export default function TextForm(props) {
     // Hooks Syntax
     const [myText, setText] = useState("");
 
-      //remove extraspace in word count
-      const countword =()=>{
-        if(myText.length>0){
-            return myText.trim().split(/[ ]+/).length;
-        }else{
-            return 0;
-        }
-    }
+      //counting words i have used one more method that is filter 
+    //   const countword =()=>{
+    //     if(myText.length>0){
+    //         return myText.trim().split(/[ ]+/).length;
+    //     }else{
+    //         return 0;
+    //     }
+    // }
 
 
     return (
@@ -94,25 +94,25 @@ export default function TextForm(props) {
             <div className="container" style={{color:props.mode ==='dark'?'#fff':'black'}}>
                 <div className="mb-3 my-3">
                     <h5 className="form-label">{props.Title}</h5>
-                    <textarea className="form-control" value={myText} onChange={handelOnChange} style={{backgroundColor:props.mode ==='dark'?'#557a9e':'white' , color:props.mode === 'dark'?'#fff':'black'} } id="myBox" rows="10"></textarea>
-                    <button className="btn btn-primary my-3" onClick={handelUpClick}>Convert To Uperrcase</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handelUpClick1}>Convert To Lowercase</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleReverse}> Reverse</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handledownload}> Download</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleBackspace}> Backspace</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleCpatalize}> Cpatalize</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleClear}> Clear Text</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleCopy}>Copy Text</button>
-                    <button className="btn btn-primary my-3 mx-1" onClick={handleRemove}>Remove Extra Space</button>
+                    <textarea className="form-control" value={myText} onChange={handelOnChange} style={{backgroundColor:props.mode ==='dark'?'#233446':'white' , color:props.mode === 'dark'?'#fff':'black'} } id="myBox" rows="10"></textarea>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3" onClick={handelUpClick}>Convert To Uperrcase</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handelUpClick1}>Convert To Lowercase</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleReverse}> Reverse</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handledownload}> Download</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleBackspace}> Backspace</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleCpatalize}> Cpatalize</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleClear}> Clear Text</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleCopy}>Copy Text</button>
+                    <button disabled = {myText.length === 0} className="btn btn-primary my-3 mx-1" onClick={handleRemove}>Remove Extra Space</button>
                 </div>
             </div>
             
             <div className="container" style={{color:props.mode ==='dark'?'#fff':'black'}}>
                 <h5>Your Summmery</h5>
-                <p>{countword()} words and {myText.length} characters.</p>
-                <p>{0.008 * myText.split(" ").length} Average time to read this one</p>
+                <p>{myText.split(" ").filter((element)=>{return element.length!== 0}).length} words and {myText.length} characters.</p>
+                <p>{0.008 * myText.split(" ").filter((element)=>{return element.length!== 0}).length} Average time to read this one</p>
                 <h5>Preview</h5>
-                <p>{myText.length>0? myText:"Enter something to preview"}</p>
+                <p>{myText.length>0? myText:"Nothing to preview ..!"}</p>
             </div>
         </>
     )
